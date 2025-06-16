6dd6a6ea3b5460c944ba8d40d18447068ececd68
aakashsaini09 <aakashsaini948585@gmail.com>
2025-06-16 17:10:31 +0530

    imagekit

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,6 +54,14 @@ callbacks: {
     }
     return session
   },
- 
-}
+},
+  pages: {
+    signIn: "/login",
+    error: "/login"
+  },
+  session:{
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60
+  },
+  secret: process.env.NEXTAUTH_SECRET
 };
