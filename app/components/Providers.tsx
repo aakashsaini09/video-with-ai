7475dcdc0933e7b0545030caa3aa4318ec0415b4
aakashsaini09 <aakashsaini948585@gmail.com>
2025-06-21 14:45:14 +0530

    file Upload

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -4,9 +4,8 @@ import { SessionProvider } from "next-auth/react"
 const urlEndPoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT;
 export default function Providers({ children}: { children: React.ReactNode}){
     return <SessionProvider refetchInterval={5 * 60}>
-        <ImageKitProvider>
-
-        {children}
+        <ImageKitProvider urlEndpoint={urlEndPoint}>
+            {children}
         </ImageKitProvider>
     </SessionProvider>
 }
